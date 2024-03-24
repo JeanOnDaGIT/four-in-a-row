11c87080b80c16af83685ec4b4f228679f6895ee
Tom <146028728+TomL000@users.noreply.github.com>
2024-03-24 13:34:55 +0000

    add title screen
    yes i am working on the code bit by bit

--- ConnectFour/MainWindow.xaml.cs
+++ ConnectFour/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ConnectFour
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
