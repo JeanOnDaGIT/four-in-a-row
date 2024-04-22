f1a1d5025b2ca9b4c222c12488e2876b62230989
Lobo <146028728+TomL000@users.noreply.github.com>
2024-04-22 22:47:43 +0100

    Grid Working
    One issue is that the tokens will not fall in the grid making it get stuck, this will need to be fixed.

--- ConnectFour/MainGame.xaml.cs
+++ ConnectFour/MainGame.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,22 +17,62 @@ namespace ConnectFour
 {
     public partial class MainGame : Window
     {
+        public enum Player
+        {
+            None, Red, Blue
+        }
+        private bool currentPlayerIsRed = true;
+        private Player[,] gameBoard = new Player[6, 7];
         public MainGame()
         {
             InitializeComponent();
 
-            // Button loop to generate the grid
+            //Button loop to generate the grid
             for (int row = 0; row < 6; row++)
             {
                 for (int col = 0; col < 7; col++)
                 {
                     Button button = new Button();
-                    button.Content = $"Cell ({row},{col})";
+                    //button.Content = $"Cell ({row},{col})"; (This tells the user which column it is)
+                    button.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    button.VerticalAlignment = VerticalAlignment.Stretch;
                     Grid.SetRow(button, row);
                     Grid.SetColumn(button, col);
                     gameGrid.Children.Add(button);
+                    button.Click += Button_Click;
+
                 }
             }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+
+            int column = Grid.GetColumn(clickedButton);
+
+            //Finds the lowest available row in the column
+            for (int row = 5; row >= 0; row--)
+            {
+                if (!IsCellOccupied(row, column))
+                {
+                    // Set the background color of the button
+                    SolidColorBrush colorBrush = currentPlayerIsRed ? Brushes.Red : Brushes.Blue;
+                    clickedButton.Background = colorBrush;
+
+                    // Update the game board state
+                    gameBoard[row, column] = currentPlayerIsRed ? Player.Red : Player.Blue;
+
+                    // Switch to the next player
+                    currentPlayerIsRed = !currentPlayerIsRed;
+                    break;
+                }
+            }
+        }
+
+        private bool IsCellOccupied(int row, int column)
+        {
+            //Checks if the cell at the specified row and column is occupied
+            return gameBoard[row, column] != Player.None;
         }
     }
 }
